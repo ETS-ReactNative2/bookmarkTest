e5ad0c5c180c8a866062716cd0a081b56543fcd5
rofiq <muhamad.rofiq@gmail.com>
2022-04-28 00:06:22 +0700

    prepare UI

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React from 'react';
-import type {Node} from 'react';
+import type { Node } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -26,87 +26,61 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import { Provider } from 'react-redux';
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
+import { createStore } from 'redux';
+import { NavigationContainer } from '@react-navigation/native';
+import Base from './Screen/Base/Base';
+import Home from './Screen/Home/Home';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const Stack = createStackNavigator();
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+const initialState = {
+  dataProfile: {},
+  initialModal: false,
+  theme: 'light',
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+}
+function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'UPDATE_INITIALMODAL':
+      const { initialModal } = action.payload;
+      return { ...state, initialModal };
+    default:
+      return state;
+  }
+}
+const store = createStore(rootReducer);
 
-export default App;
+export default class App extends React.Component {
+
+  render() {
+
+    return (
+      // <Root>
+      <SafeAreaProvider>
+        <Provider store={store}>
+          <NavigationContainer>
+            <Stack.Navigator
+              screenOptions={{
+                gestureEnabled: true,
+                cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS
+              }}
+            >
+              <Stack.Screen
+                name="Home"
+                component={Home}
+                options={{
+                  headerShown: false
+                }}
+              />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </Provider>
+      </SafeAreaProvider>
+      // </Root>
+    )
+  }
+}
